bc3a6f295fbff3242991406f9e7adeec732f5995
olrik-web <molrik@outlook.com>
2021-04-15 14:49:06 +0200

    en masse quiz css og html

--- src/App.js
+++ src/App.js
@@ -5,6 +5,51 @@ function App() {
       <header className="App-header">
         <h1>Quiz app</h1>
       </header>
+      <table>
+        <tr>
+          <td colSpan="2" >
+            <h2 className="category">Category</h2>
+            <p>Points: 0</p>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan="2">
+            <h3 className="question">Question</h3>
+          </td>
+        </tr>
+        <tr>
+          <td className="answer">
+            <p>Answer 1</p>
+          </td>
+          <td className="answer">
+            <p>Answer 2</p>
+          </td>
+        </tr>
+        <tr>
+          <td className="answer">
+            <p>Answer 3</p>
+          </td>
+          <td className="answer">
+            <p>Answer 4</p>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan="2">
+            <p>Information <br />
+            Source: <a href="">Link</a>
+            </p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button">Button</button>
+          </td>
+          <td>
+            <button type="button">Button</button>
+          </td>
+
+        </tr>
+      </table>
 
     </div>
   );
